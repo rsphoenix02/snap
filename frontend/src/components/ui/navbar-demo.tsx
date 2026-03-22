d929a9f408a041094167796a19e87fe57dda8b93
rsphoenix02 <saq.saf0501@gmail.com>
2026-03-22 20:35:04 +0530

    chore: remove unused demo files and duplicate button.tsx

--- frontend/src/components/ui/navbar-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "@/components/ui/navbar";
-
-export default function DemoOne() {
-  return (
-    <div className="mx-auto max-w-5xl flex justify-center">
-    <Header />
-  </div>
-  );
-}
